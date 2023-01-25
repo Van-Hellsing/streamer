ee1a1225d7045c6d89bea2701e5697affd3ed34a
Van-Hellsing <forum.mail.temp@gmail.com>
2023-01-25 11:16:42 +0300

    dependencies removed

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 Автоматическая базовая настройка и запуск потоков с камеры или камер с помощью [uStreamer](https://github.com/pikvm/ustreamer) для веб интефейса [klipper](https://github.com/Klipper3d/klipper).
 
-Зависит от [kiauh](https://github.com/th33xitus/kiauh),он должен быть установлен, испрльзуются файлы [globals.sh](https://github.com/th33xitus/kiauh/blob/master/scripts/globals.sh) и [utilities.sh](https://github.com/th33xitus/kiauh/blob/master/scripts/utilities.sh).
-
 При обнаружении новой камеры в папке *~/streamer/webcam\_config* создаётся файл настроек, *webcam\_\<port\>\_\<cam_name\>\_\<by-path\>.cfg*, содержащий минимум настроек.
 В папке настроек принтера/принтеров создаётся ссылка на папку с файлами настроек камеры/камер.
+
+Имена принтеров извлекаются из файла настроек [kiauh](https://github.com/th33xitus/kiauh).
 
 Порт трансляции изменяется простым переименованием файла настроек. При переименовании следите за тем чтобы выделеный диапазон был непрерывным и начинался с 8080.
 
--- start.sh
+++ start.sh
@@ -3,10 +3,79 @@
 set -e
 
 source ${HOME}/streamer/streamer.sh
-source ${HOME}/kiauh/scripts/globals.sh
-source ${HOME}/kiauh/scripts/utilities.sh
 
-set_globals
+### kiauh function
+
+### kiauh color
+green=$(echo -en "\e[92m")
+yellow=$(echo -en "\e[93m")
+magenta=$(echo -en "\e[35m")
+red=$(echo -en "\e[91m")
+cyan=$(echo -en "\e[96m")
+white=$(echo -en "\e[39m")
+
+### kiauh msg
+function status_msg() {
+  echo -e "\n${magenta}###### ${1}${white}"
+}
+function ok_msg() {
+  echo -e "${green}[✓ OK] ${1}${white}"
+}
+function title_msg() {
+  echo -e "${cyan}${1}${white}"
+}
+function print_error() {
+  [[ -z ${1} ]] && return
+
+  echo -e "${red}"
+  echo -e "#=======================================================#"
+  echo -e " ${1} "
+  echo -e "#=======================================================#"
+  echo -e "${white}"
+}
+function print_confirm() {
+  [[ -z ${1} ]] && return
+
+  echo -e "${green}"
+  echo -e "#=======================================================#"
+  echo -e " ${1} "
+  echo -e "#=======================================================#"
+  echo -e "${white}"
+}
+
+function dependency_check() {
+  local dep=( "${@}" )
+  local packages
+  status_msg "Checking for the following dependencies:"
+
+  #check if package is installed, if not write its name into array
+  for pkg in "${dep[@]}"; do
+    echo -e "${cyan}● ${pkg} ${white}"
+    [[ ! $(dpkg-query -f'${Status}' --show "${pkg}" 2>/dev/null) = *\ installed ]] && \
+    packages+=("${pkg}")
+  done
+
+  #if array is not empty, install packages from array
+  if (( ${#packages[@]} > 0 )); then
+    status_msg "Installing the following dependencies:"
+    for package in "${packages[@]}"; do
+      echo -e "${cyan}● ${package} ${white}"
+    done
+    echo
+
+    if sudo apt-get update --allow-releaseinfo-change && sudo apt-get install "${packages[@]}" -y; then
+      ok_msg "Dependencies installed!"
+    else
+      error_msg "Installing dependencies failed!"
+      return 1 # exit kiauh
+    fi
+  else
+    ok_msg "Dependencies already met!"
+    return
+  fi
+}
+
+### kiauh functions end
 
 function menu(){
 
